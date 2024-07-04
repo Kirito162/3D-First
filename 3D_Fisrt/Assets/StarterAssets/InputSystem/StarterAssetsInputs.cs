729137b6fd8e14280dafac175daf271635ff5b72
Kirito162 <huynhlengockhiem@gmail.com>
2024-07-04 11:22:42 +0700

    Done base
    Continue the project

---
+++ 3D_Fisrt/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+		public bool play;
+		public bool skill1;
+		public bool skill2;
+        public bool skill3;
+
+        [Header("Movement Settings")]
+		public bool analogMovement;
+
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+        public void OnPlay(InputValue value)
+        {
+            PlayInput(value.isPressed);
+        }
+
+        public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+        public void OnSkill_1(InputValue value)
+        {
+            Skill_1Input(value.isPressed);
+        }
+#endif
+
+
+        public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+
+        public void PlayInput(bool newPlayState)
+        {
+            play = newPlayState;
+        }
+
+        public void Skill_1Input(bool newSkill1State)
+        {
+            skill1 = newSkill1State;
+        }
+        private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+	}
+	
+}
